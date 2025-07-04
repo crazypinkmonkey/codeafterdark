2c916a923df5f677d60a3075c1c1078ab4ce1fbc
crazypinkmonkey <p75196156@gmail.com>
2025-07-04 12:47:47 -0700

    Update Test_llama.py

--- Test_llama.py
+++ Test_llama.py
@@ -1,6 +1,6 @@
 from llama_cpp import Llama
 
-model_path = "C:/Users/Aditya/models/tinyllama-1.1b-chat-v1.0.Q4_0.gguf"
+model_path = "model_path"
 llm = Llama(model_path=model_path, n_ctx=512)
 
 prompt = "<|system|>\nYou are a helpful assistant.<|end|>\n<|user|>\nWhat is the capital of India?<|end|>\n<|assistant|>"
